cc14a07e4ea3db46e012df598dd2dcf9e9c548c2
malina <karjavina.alina@yandex.ru>
2021-02-25 19:18:07 +0300

    notepad v.1

--- post.rb
+++ post.rb
@@ -1,4 +1,13 @@
 class Post
+
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
+  end
+
   def initialize
     @created_at = Time.now
     @text = nil
@@ -26,6 +35,6 @@ class Post
 
     file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
 
-    return current_path + "/" + file_name
+    return current_path + "/data" + "/" + file_name
   end
 end
--- task.rb
+++ task.rb
@@ -1,3 +1,5 @@
+require "date"
+
 class Task < Post
   def initialize
     super
@@ -6,8 +8,20 @@ class Task < Post
   end
 
   def read_from_console
+    puts "Что надо сделать?"
+    @text = STDIN.gets.chomp
+
+    puts "К какому числу? Укажите дату в формате ДД.ММ.ГГГГ."
+    input = STDIN.gets.chomp
+
+    @due_date = Date.parse(input)
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n \n"
+
+    dead_line = "Крайний срок #{@due_date}"
+
+    return [dead_line, @text, time_string]
   end
 end
